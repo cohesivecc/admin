92cde7c7cae3c7f8cf63d99a42b5d6837a970977
Nick Mixdorf <namixdorf@gmail.com>
2017-06-13 16:38:09 -0500

    updated to try with a redirect

--- app/controllers/cohesive_admin/dashboard_controller.rb
+++ app/controllers/cohesive_admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 module CohesiveAdmin
   class DashboardController < CohesiveAdmin::BaseController
     def index
-       @header = "Dashboard"
+       redirect_to new_session_path
      end
 	end
 end
