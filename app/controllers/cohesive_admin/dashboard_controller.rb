65cbd59ecf660960345e2522929bac1889d77c1b
Nick Mixdorf <namixdorf@gmail.com>
2017-06-14 10:01:24 -0500

    Added a default dashboard for hillside

--- app/controllers/cohesive_admin/dashboard_controller.rb
+++ app/controllers/cohesive_admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 module CohesiveAdmin
   class DashboardController < CohesiveAdmin::BaseController
-		
+    def index
+      @dashboard = OpenStruct.new(
+                              title: 'Welcome'
+      )
+    end
 	end
 end
