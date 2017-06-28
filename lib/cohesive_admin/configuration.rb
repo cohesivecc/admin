7880bdf2003a17788657c56c0168b9363f4b85ea
Nick Mixdorf <namixdorf@gmail.com>
2017-06-28 12:22:17 -0500

    remove after config call

--- lib/cohesive_admin/configuration.rb
+++ lib/cohesive_admin/configuration.rb
@@ -1,18 +1,18 @@
 module CohesiveAdmin
 	class Configuration
-		
+
 		DEFAULT_SETTINGS = {
 			namespace: :admin,
 			default_sort_field: :position
 		}.freeze
-		
+
 		attr_accessor :namespace
 		attr_accessor :default_sort_field
 		attr_accessor :dashboards
 		attr_accessor :aws
 		attr_accessor :froala
 		attr_accessor :authentication
-		
+
 		def initialize
 			# set up defaults
 			@namespace					= :admin
@@ -21,10 +21,10 @@ module CohesiveAdmin
 			@aws								= nil
 			@froala							= nil
 			@authentication			= CohesiveAdmin::Authentication
-			
+
 			load_model_config
 		end
-		
+
 		def [](path)
 			parts = path.split('.')
 			thing = self.send(parts.shift)
@@ -44,16 +44,16 @@ module CohesiveAdmin
 				self.config.aws[:access_key_id]     ||= (self.config.aws[:credentials].credentials.access_key_id rescue nil)
 			end
 		end
-		
+
 	private
-		
+
 		def load_model_config
 			filename = File.join("config", "cohesive_admin.yml")
 			load_paths = [
 				CohesiveAdmin::Engine.root,
 				Rails.root
 			]
-			
+
 			@dashboards = {}.with_indifferent_access
 			load_paths.each do |path|
 				config_file = path.join(filename)
@@ -63,23 +63,21 @@ module CohesiveAdmin
 				end
 			end
 		end
-	
+
 	end
-	
+
 	def self.config
 		@configuration ||= Configuration.new
-
-		after_configure
 	end
-	
+
 	def self.config=(config)
 		@configuration = config
 	end
-	
+
 	def self.configure
 		yield config
 	end
 
 
-	
+
 end
