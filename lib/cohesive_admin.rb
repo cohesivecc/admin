13ead3322d263de55f647d6ec698f57f41fa0a9c
Nick Mixdorf <namixdorf@gmail.com>
2017-06-28 14:12:17 -0500

    updated the froala settings

--- lib/cohesive_admin.rb
+++ lib/cohesive_admin.rb
@@ -11,17 +11,17 @@ require "cohesive_admin/dashboard"
 require "cohesive_admin/engine"
 
 module CohesiveAdmin
-	
+
 	def self.namespace
 		Rails.application.routes.named_routes[:cohesive_admin].path.source.sub("\\A/", "").to_sym
 	end
-	
+
 	@@router = nil
 	def self.routing
 		@@router ||= CohesiveAdmin::Routing.new
 	end
-	
-	
+
+
 	def self.as_json(options={})
 		# pass CohesiveAdmin config settings down to Javascript
 		js_config = {
@@ -30,8 +30,30 @@ module CohesiveAdmin
 			froala: { key:(config.froala[:key] rescue nil) },
 			mount_point: "/#{ namespace }"
 		}
-	
+
+    # AWS settings
+    unless self.config.aws.blank?
+      # Froala requires that we provide the region-specific endpoint as the 'region' parameter. We'll use the aws-sdk gem to provide this.
+      bucket = Aws::S3::Bucket.new(self.config.aws[:bucket], { region: self.config.aws[:region] })
+      region = bucket.client.config.endpoint.host.gsub(/\.amazonaws\.com\Z/, '')
+
+      js_config[:aws] = {
+          bucket:         self.config.aws[:bucket],
+          region:         region,
+          key_start:      self.config.aws[:key_start],
+          acl:            self.config.aws[:acl],
+          access_key_id:  self.config.aws[:access_key_id],
+          policy:         AmazonSignature.policy,
+          signature:      AmazonSignature.signature,
+          assets:         {
+                            index:      Engine.routes.url_helpers.s3_assets_path,
+                            delete:     Engine.routes.url_helpers.delete_s3_assets_path,
+                            preloader:  ActionController::Base.helpers.asset_path('cohesive_admin/preloader.gif')
+                          }
+      }
+    end
+
 		js_config
 	end
-	
+
 end
